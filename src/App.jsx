a74567d5e65d7c9fd38db91e94dd8475ae74256c
Reza Zafarian <92252207+LixCp@users.noreply.github.com>
2024-07-13 11:12:22 +0000

    paginations

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,89 @@
-import './App.css';
+import React, { useEffect, useState } from "react";
 
-function App() {
+export default function App() {
+  const [todos, setTodos] = useState([]);
+  const [currentPges, setCurrentPges] = useState(1);
+  const [paginatedTodos, setPaginatedTodos] = useState([]);
+
+  let pagesSize = 10;
+  
+  const pagesCount = Math.ceil(todos.length / pagesSize);
+  const pagesNumber = Array.from(Array(pagesCount).keys());
+
+  useEffect(() => {
+    let endIndex = pagesSize * currentPges;
+    let startIndex = endIndex - pagesSize;
+    let allShownTodos = todos.slice(startIndex, endIndex);
+    setPaginatedTodos(allShownTodos);
+  }, [currentPges]);
+
+  const changePaginate = (newPage)=>{
+    setCurrentPges(newPage)
+
+  }
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => res.json())
+      .then((datas) => {
+        setTodos(datas);
+        let endIndex = pagesSize * currentPges
+        let startIndex = endIndex - pagesSize
+        let allShowTodos = datas.slice(startIndex,endIndex)
+        setPaginatedTodos(allShowTodos)  
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+    <div>
+      {!todos ? (
+        "Loading"
+      ) : (
+        <table className="table">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>User ID</th>
+              <th>Title</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {paginatedTodos.map((item) => (
+              <tr key={item.id}>
+                <td>{item.id}</td>
+                <td>{item.userId}</td>
+                <td>{item.title}</td>
+                <td>
+                  <p
+                    className={
+                      item.completed ? "btn btn-success" : "btn btn-danger"
+                    }
+                  >
+                    {item.completed ? "Completed" : "Pending"}
+                  </p>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+      <nav className="d-flex justify-content-center" aria-label="...">
+        <ul className="pagination pagination-md">
+          {pagesNumber.map((item) => (
+            <li
+              key={item + 1}
+              className={
+                item + 1 === currentPges ? "active page-item" : "page-item"
+              }
+              onClick={()=>changePaginate(item+1)}
+              aria-current="page"
+            >
+              <span className="page-link">{item + 1}</span>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </div>
   );
 }
-
-export default App;
